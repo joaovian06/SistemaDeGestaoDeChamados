bb4991489e3673c29e7a8fda5e6a3ad63de70fc7
joaovian06 <joaovitor_vian@hotmail.com>
2022-06-07 08:05:23 -0300

    add more treatments type to treatment model

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -5,7 +5,7 @@ class Treatment < ApplicationRecord
   # belongs_to :consultant, inverse_of: :treatments
   # belongs_to :customer, inverse_of: :treatments
 
-  enum treatment_type: [:password_reset, :software ]
+  enum treatment_type: [:password_reset, :software, :hardware_replacement, :hardware_setup, :misconnection, :user_setup, :user_remove ]
   enum status: [:open, :in_progress, :waiting, :done]
 
   validates_length_of :description, minimum: 20, on: :create
